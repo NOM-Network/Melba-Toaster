133b9e5b6a2bb6740e13937b9fec24fa9646f210
Haram Bae <kotgedev@gmail.com>
2023-10-21 01:57:19 +0900

    first commit

---
+++ scripts/main.gd
@@ -0,0 +1,30 @@
+extends Control
+
+@export var path = "C:/Users/user/Downloads/audio/" 
+
+@onready var model = $Model
+@onready var server = $WebSocketServer
+
+const PORT = 8765 
+
+var id: int 
+
+func _ready() -> void:
+    get_viewport().transparent_bg = true
+    
+    var err: Error = server.listen(PORT)
+    if err != OK:
+        print("Error listing on port %s" % PORT)
+    
+func _on_web_socket_server_message_received(peer_id, message):
+    server.send(id, message)
+    if message == "quit":
+        get_tree().quit()
+    if message == "toast":
+        model.toast_toggle()
+    if message.is_valid_int():
+        model.play_audio(path + message + ".wav")
+
+func _on_web_socket_server_client_connected(peer_id):
+    id = peer_id 
+    print(peer_id)
---
+++ scripts/model.gd
@@ -0,0 +1,83 @@
+extends Node2D
+
+@onready var mouth_closed = $MouthClosed
+@onready var mouth_open = $MouthOpen
+@onready var toast = $Toast
+@onready var audio_player = $AudioStreamPlayer
+@onready var mouth_animation_player = $MouthAnimationPlayer
+@onready var blink_animation_player = $BlinkAnimationPlayer
+@onready var reading_animation_player = $ReadingAnimationPlayer
+
+enum States {
+    LOOKINGSTRAIGHT,
+    READINGCHAT
+}
+
+var state: States
+
+func _ready() -> void:
+    look_at_chat()
+    pass
+
+func _process(delta: float) -> void:
+    if state == States.LOOKINGSTRAIGHT:
+        var volume = (AudioServer.get_bus_peak_volume_left_db(0,0) + AudioServer.get_bus_peak_volume_right_db(0,0)) / 2.0
+        if volume < -60.0:
+            close_mouth()
+        else:
+            blabber_mouth()
+
+
+# Functions that needs to be able to be called from a python script 
+func play_audio(path: String) -> void:
+    look_straight()
+    
+    # Get file 
+    var file = FileAccess.open(path, FileAccess.READ)
+    var buffer = file.get_buffer(file.get_length())
+    file.close()
+    
+    # Get bytes
+    var stream = AudioStreamWAV.new()
+    stream.data = buffer
+    stream.format = AudioStreamWAV.FORMAT_16_BITS
+    
+    # Play sound and animations 
+    audio_player.stream = stream
+    audio_player.play()
+    blabber_mouth()
+
+func toast_toggle() -> void: 
+    if toast.visible:
+        toast.visible = false
+    else:
+        toast.visible = true 
+
+
+# Functions for movement and animation 
+func look_straight() -> void: 
+    if state != States.LOOKINGSTRAIGHT:
+        state = States.LOOKINGSTRAIGHT
+        reading_animation_player.play_backwards("look_at_chat") 
+
+func look_at_chat() -> void:
+    reading_animation_player.play("look_at_chat")
+    state = States.READINGCHAT
+
+func close_mouth() -> void:
+    mouth_animation_player.stop()
+    mouth_closed.visible = true
+    mouth_open.visible = false
+
+func blabber_mouth() -> void:
+    mouth_animation_player.play("mouth")
+    
+func blink() -> void:
+    blink_animation_player.play("blink")
+
+func _on_audio_stream_player_finished() -> void:
+    look_at_chat()
+    close_mouth()
+
+func _on_blink_timer_timeout():
+    blink()
