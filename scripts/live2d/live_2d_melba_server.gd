bfa3eacc19d30ca34435a67b16ebe293dc5b75a2
KotgeDev <68812485+KotgeDev@users.noreply.github.com>
2023-10-22 02:02:48 +0900

    spaces to tabs

--- scripts/live2d/live_2d_melba_server.gd
+++ scripts/live2d/live_2d_melba_server.gd
@@ -9,22 +9,22 @@ const PORT = 8765
 var id: int 
 
 func _ready() -> void:
-    # Get rid of this code when we have a background. 
-    get_tree().get_root().set_transparent_background(true)
-    DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_TRANSPARENT, true, 0)
+	# Get rid of this code when we have a background. 
+	get_tree().get_root().set_transparent_background(true)
+	DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_TRANSPARENT, true, 0)
 
-    var err: Error = server.listen(PORT)
-    if err != OK:
-        print("Error listing on port %s" % PORT)
-    
+	var err: Error = server.listen(PORT)
+	if err != OK:
+		print("Error listing on port %s" % PORT)
+	
 func _on_web_socket_server_message_received(peer_id, message):
-    server.send(id, message)
-    if typeof(message) == 4: 
-        var data = JSON.parse_string(message)
-        if data["type"] == "PlayAnimation":
-            if data["animationName"] == "wink1":
-                model.test()
-                
+	server.send(id, message)
+	if typeof(message) == 4: 
+		var data = JSON.parse_string(message)
+		if data["type"] == "PlayAnimation":
+			if data["animationName"] == "wink1":
+				model.test()
+				
 func _on_web_socket_server_client_connected(peer_id):
-    id = peer_id 
-    print(peer_id)
+	id = peer_id 
+	print(peer_id)
--- scripts/pixel/pixel_melba.gd
+++ scripts/pixel/pixel_melba.gd
@@ -9,75 +9,75 @@ extends Node2D
 @onready var reading_animation_player = $ReadingAnimationPlayer
 
 enum States {
-    LOOKINGSTRAIGHT,
-    READINGCHAT
+	LOOKINGSTRAIGHT,
+	READINGCHAT
 }
 
 var state: States
 
 func _ready() -> void:
-    look_at_chat()
-    pass
+	look_at_chat()
+	pass
 
 func _process(_delta: float) -> void:
-    if state == States.LOOKINGSTRAIGHT:
-        var volume = (AudioServer.get_bus_peak_volume_left_db(0,0) + AudioServer.get_bus_peak_volume_right_db(0,0)) / 2.0
-        if volume < -60.0:
-            close_mouth()
-        else:
-            blabber_mouth()
+	if state == States.LOOKINGSTRAIGHT:
+		var volume = (AudioServer.get_bus_peak_volume_left_db(0,0) + AudioServer.get_bus_peak_volume_right_db(0,0)) / 2.0
+		if volume < -60.0:
+			close_mouth()
+		else:
+			blabber_mouth()
 
 
 # Functions that needs to be able to be called from a python script 
 func play_audio(path: String) -> void:
-    look_straight()
-    
-    # Get file 
-    var file = FileAccess.open(path, FileAccess.READ)
-    var buffer = file.get_buffer(file.get_length())
-    file.close()
-    
-    # Get bytes
-    var stream = AudioStreamWAV.new()
-    stream.data = buffer
-    stream.format = AudioStreamWAV.FORMAT_16_BITS
-    
-    # Play sound and animations 
-    audio_player.stream = stream
-    audio_player.play()
-    blabber_mouth()
+	look_straight()
+	
+	# Get file 
+	var file = FileAccess.open(path, FileAccess.READ)
+	var buffer = file.get_buffer(file.get_length())
+	file.close()
+	
+	# Get bytes
+	var stream = AudioStreamWAV.new()
+	stream.data = buffer
+	stream.format = AudioStreamWAV.FORMAT_16_BITS
+	
+	# Play sound and animations 
+	audio_player.stream = stream
+	audio_player.play()
+	blabber_mouth()
 
 func toast_toggle() -> void: 
-    if toast.visible:
-        toast.visible = false
-    else:
-        toast.visible = true 
+	if toast.visible:
+		toast.visible = false
+	else:
+		toast.visible = true 
 
 
 # Functions for movement and animation 
 func look_straight() -> void: 
-    if state != States.LOOKINGSTRAIGHT:
-        state = States.LOOKINGSTRAIGHT
-        reading_animation_player.play_backwards("look_at_chat") 
+	if state != States.LOOKINGSTRAIGHT:
+		state = States.LOOKINGSTRAIGHT
+		reading_animation_player.play_backwards("look_at_chat") 
 
 func look_at_chat() -> void:
-    reading_animation_player.play("look_at_chat")
-    state = States.READINGCHAT
+	reading_animation_player.play("look_at_chat")
+	state = States.READINGCHAT
 
 func close_mouth() -> void:
-    mouth_animation_player.stop()
-    mouth_closed.visible = true
-    mouth_open.visible = false
+	mouth_animation_player.stop()
+	mouth_closed.visible = true
+	mouth_open.visible = false
 
 func blabber_mouth() -> void:
-    mouth_animation_player.play("mouth")
-    
+	mouth_animation_player.play("mouth")
+	
 func blink() -> void:
-    blink_animation_player.play("blink")
+	blink_animation_player.play("blink")
 
 func _on_audio_stream_player_finished() -> void:
-    look_at_chat()
-    close_mouth()
+	look_at_chat()
+	close_mouth()
 
 func _on_blink_timer_timeout():
-    blink()
+	blink()
