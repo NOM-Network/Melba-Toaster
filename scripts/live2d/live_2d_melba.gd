3408dca610c7807f109770f4988e08ea12be62c0
KotgeDev <68812485+KotgeDev@users.noreply.github.com>
2023-10-24 22:29:11 +0900

    Naming changes

--- scripts/live2d/live_2d_melba.gd
+++ scripts/live2d/live_2d_melba.gd
@@ -1,38 +1,64 @@
 extends MelbaModel
 
-@onready var cubism_model = $Sprite2D/GDCubismUserModel
+@onready var cubism_model = $"../Live2DMelba/Sprite2D/GDCubismUserModel"
+@onready var mouth_movement = $"../Live2DMelba/Sprite2D/GDCubismUserModel/MouthMovement"
+@onready var audio_player = $"../AudioStreamPlayer"
 
+# Toggles 
+var toast_toggled = true 
+
+# Expressions 
 enum Expressions {
-	TOAST,
-	NO_TOAST
+	SMILE
 }
+var current_expression = Expressions.SMILE
 
-var current_expression = Expressions.NO_TOAST
+# Animations 
+enum Animations {
+	LOOKING_STRAIGHT, 
+	LOOKING_AT_CHAT 
+}
+var current_animation = Animations.LOOKING_AT_CHAT 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	set_process(false)
-
 	connect_signals()
 	cubism_model.motion_finished.connect(_on_gd_cubism_user_model_motion_finished)
-
+	audio_player.finished.connect(_on_audio_stream_player_finished)
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-# func _process(_delta: float) -> void:
-# 	pass
+func _process(_delta: float) -> void:
+	if current_animation == Animations.LOOKING_AT_CHAT: 
+		if audio_queue.size() > 0: 
+			play_audio(audio_queue[0])
+			audio_queue.remove_at(0)
 
-func look_at_chat() -> void:
-	cubism_model.start_motion("", 4, GDCubismUserModel.PRIORITY_FORCE)
+func look_straight() -> void:
+	current_animation = Animations.LOOKING_STRAIGHT
+#	cubism_model.start_motion("", 4, GDCubismUserModel.PRIORITY_FORCE)
+
+func look_at_chat() -> void: 
+	current_animation = Animations.LOOKING_AT_CHAT 
 
 func toast_toggle() -> void:
-	if current_expression != Expressions.TOAST:
-		current_expression = Expressions.TOAST
+	if not toast_toggled:
+		toast_toggled = true 
 		cubism_model.start_expression("exp_04")
 	else:
-		current_expression = Expressions.NO_TOAST
+		toast_toggled = false 
 		cubism_model.start_expression("exp_01")
+
+func play_audio(stream: AudioStreamWAV) -> void:
+	look_straight()
+	
+	audio_player.stream = stream
+	audio_player.play()
 
 func idle_animation() -> void:
 	cubism_model.start_motion("Idle", 0, GDCubismUserModel.PRIORITY_FORCE)
 
 func _on_gd_cubism_user_model_motion_finished():
 	idle_animation()
+
+func _on_audio_stream_player_finished():
+	look_at_chat()  
