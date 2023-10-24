3a6e7b9cf6a347c8be84010c91fc343ed5c54f9e
KotgeDev <68812485+KotgeDev@users.noreply.github.com>
2023-10-25 02:09:03 +0900

    Updated live2d model

--- scripts/live2d/live_2d_melba.gd
+++ scripts/live2d/live_2d_melba.gd
@@ -6,25 +6,18 @@ extends MelbaModel
 
 var reading_audio = false 
 
-# Expressions 
-enum Expressions {
-	NULL,
-	TOAST,
-	VOID,
-}
-var current_expression = Expressions.NULL
-
 # Animations 
-enum Animations {
-	IDLE
-}
-var current_animation = Animations.IDLE
+#enum Animations {
+#	IDLE
+#}
+#var current_animation = Animations.IDLE
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	connect_signals()
 	cubism_model.motion_finished.connect(_on_gd_cubism_user_model_motion_finished)
 	audio_player.finished.connect(_on_audio_stream_player_finished)
+	var parameters = cubism_model.get_parameters()
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
