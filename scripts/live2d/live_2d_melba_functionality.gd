3476ef208bbc3d5e6884ca501f8ba19fdc40705d
KotgeDev <68812485+KotgeDev@users.noreply.github.com>
2023-10-24 22:11:11 +0900

    Live2DMelba: Made her mouth work

--- scripts/live2d/live_2d_melba_functionality.gd
+++ scripts/live2d/live_2d_melba_functionality.gd
@@ -1,6 +1,7 @@
 extends MelbaModel
 
 @onready var cubism_model = $"../Live2DMelba/Sprite2D/GDCubismUserModel"
+@onready var mouth_movement = $"../Live2DMelba/Sprite2D/GDCubismUserModel/MouthMovement"
 @onready var audio_player = $"../AudioStreamPlayer"
 
 # Toggles 
@@ -14,14 +15,13 @@ var current_expression = Expressions.SMILE
 
 # Animations 
 enum Animations {
-	LOOKING_STRAIGHT, # For when playing an audio  
-	LOOKING_AT_CHAT # For when not playing an audio 
+	LOOKING_STRAIGHT, 
+	LOOKING_AT_CHAT 
 }
 var current_animation = Animations.LOOKING_AT_CHAT 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print(cubism_model.get_parameters())
 	connect_signals()
 	cubism_model.motion_finished.connect(_on_gd_cubism_user_model_motion_finished)
 	audio_player.finished.connect(_on_audio_stream_player_finished)
@@ -30,7 +30,6 @@ func _ready() -> void:
 func _process(_delta: float) -> void:
 	if current_animation == Animations.LOOKING_AT_CHAT: 
 		if audio_queue.size() > 0: 
-			print("playing audio... ")
 			play_audio(audio_queue[0])
 			audio_queue.remove_at(0)
 
@@ -53,12 +52,7 @@ func play_audio(stream: AudioStreamWAV) -> void:
 	look_straight()
 	
 	audio_player.stream = stream
-	audio_player.play()  
-	
-	blabber_mouth()
-
-func blabber_mouth() -> void: 
-	pass 
+	audio_player.play()
 
 func idle_animation() -> void:
 	cubism_model.start_motion("Idle", 0, GDCubismUserModel.PRIORITY_FORCE)
---
+++ scripts/live2d/mouth_movement.gd
@@ -0,0 +1,63 @@
+# SPDX-License-Identifier: MIT
+# SPDX-FileCopyrightText: 2023 MizunagiKB <mizukb@live.jp>
+extends GDCubismEffectCustom
+
+
+@export var param_mouth_name: String = "ParamMouthOpenY"
+@export var param_mouth_form_name: String = "ParamMouthForm"
+
+var param_mouth: GDCubismParameter
+var param_mouth_form: GDCubismParameter
+var tween: Tween
+var blabbering = false 
+
+func _ready(): 
+	self.cubism_init.connect(_on_cubism_init)
+	self.cubism_process.connect(_on_cubism_process)
+	self.cubism_term.connect(_on_cubism_term)
+
+func _on_cubism_init(model: GDCubismUserModel):	
+	param_mouth = null
+	var ary_param = model.get_parameters()
+
+	for param in ary_param:
+#		print(param.id)
+		if param.id == param_mouth_name:
+			param_mouth = param
+		if param.id == param_mouth_form_name:
+			param_mouth_form = param 
+	
+	param_mouth_form.value = 1.0
+	print(param_mouth_form.value)
+
+func _on_cubism_term(model: GDCubismUserModel):
+	param_mouth = null
+	param_mouth_form = null 
+
+func _on_cubism_process(model: GDCubismUserModel, delta: float):		
+	if param_mouth != null:	
+		var volume = (AudioServer.get_bus_peak_volume_left_db(0,0) + AudioServer.get_bus_peak_volume_right_db(0,0)) / 2.0	
+		if volume < -60.0: # If she is not speaking 
+			blabbering = false
+		elif not blabbering: # If she just started speaking 
+			blabbering = true 
+			start_tween() 
+		else: # If she has been speaking  
+			pass
+
+func start_tween() -> void: 
+	if tween:
+		tween.kill()
+	tween = create_tween() 
+	tween.finished.connect(_on_tween_finished)
+	tween.tween_property(param_mouth, "value", 0.1, 0.15)
+
+func _on_tween_finished() -> void: 
+	if tween: 
+		tween.kill()
+	tween = create_tween()
+	await tween.tween_property(param_mouth, "value", 0.0, 0.15).finished
+	if blabbering: 
+		start_tween() 
+	else:
+		param_mouth.value = 0.0
