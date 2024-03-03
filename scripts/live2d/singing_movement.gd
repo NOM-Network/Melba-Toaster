aad795d264033b57c41ef80f4a40183853f74f84
CJMAXiK <me@cjmaxik.com>
2024-03-03 23:41:09 +0300

    Live2D: more dancing animations

--- scripts/live2d/singing_movement.gd
+++ scripts/live2d/singing_movement.gd
@@ -8,54 +8,136 @@ var bob_interval = 0.5
 var motion_range: float = 30.0
 
 # Parameters
+var param_angle_x: GDCubismParameter
 var param_angle_y: GDCubismParameter
+var param_angle_z: GDCubismParameter
+var param_body_angle_x: GDCubismParameter
 var param_body_angle_y: GDCubismParameter
+var param_body_angle_z: GDCubismParameter
 
 # Tweens
-var tween: Tween
+var tween: Dictionary
+var current_motion: Array
+
+# Timer
+@onready var beats_timer := Timer.new()
 
 func _ready():
 	self.cubism_init.connect(_on_cubism_init)
+
+	add_child(beats_timer)
+	beats_timer.timeout.connect(_on_beats_timer_timeout)
 
 func _on_cubism_init(model: GDCubismUserModel):
 	Globals.start_dancing_motion.connect(_start_motion)
 	Globals.end_dancing_motion.connect(_end_motion)
 
-	for param in model.get_parameters():
-		if param.id == "ParamAngleY":
-			param_angle_y = param
-		if param.id == "ParamBodyAngleY":
-			param_body_angle_y = param
+	var param_names = [
+		"ParamAngleX",
+		"ParamAngleY",
+		"ParamAngleZ",
+		"ParamBodyAngleX",
+		"ParamBodyAngleY",
+		"ParamBodyAngleZ"
+	]
 
-		if param_angle_y && param_body_angle_y:
-			break
+	for param in model.get_parameters():
+		if param_names.has(param.id):
+			set(param.id.to_snake_case(), param)
 
 func _start_motion(bpm: float) -> void:
 	Globals.dancing_bpm = bpm
 	bob_interval = motion_range / bpm
 
-	start_tween()
+	_start_tween()
+	_start_timer()
 
 func _end_motion() -> void:
 	Globals.dancing_bpm = 0
 
-	if tween:
-		tween.kill()
+	_stop_timer()
+	for axis in ["x", "y", "z"]:
+		if tween.has(axis) and tween[axis].is_running():
+			_stop(axis)
 
-	tween = create_tween()
-	tween.tween_property(param_angle_y, "value", 0, bob_interval)
-	tween.set_parallel().tween_property(param_body_angle_y, "value", 0, bob_interval)
+func _start_tween() -> void:
+	var new_motion = _random_motion()
+	for axis in new_motion:
+		_dance(axis)
 
-func start_tween() -> void:
-	if tween:
-		tween.kill()
+func _wait_time() -> float:
+	return ((60.0 / Globals.dancing_bpm) * 8.0) + Globals.get_audio_compensation()
 
-	tween = create_tween().set_loops()
+func _start_timer() -> void:
+	beats_timer.wait_time = _wait_time()
+	beats_timer.start()
+
+func _stop_timer() -> void:
+	beats_timer.stop()
+
+func _on_beats_timer_timeout() -> void:
+	beats_timer.wait_time = _wait_time()
+
+	if randf() < 0.33:
+		var new_motion = _random_motion()
+		if current_motion == new_motion:
+			return
+
+		current_motion = new_motion
+		for axis in ["x", "y", "z"]:
+			if axis in new_motion:
+				if not tween.has(axis) or not tween[axis].is_running():
+					_dance(axis)
+			else:
+				if tween.has(axis) and tween[axis].is_running():
+					_stop(axis)
+
+func _dance(axis: String) -> void:
+	var param := "param_angle_" + axis
+	var body_param := "param_body_angle_" + axis
+
+	var time_modifier := _time_modifier(axis)
+	var motion_modifier := 1.0
+
+	if tween.has(axis):
+		tween[axis].kill()
+
+	tween[axis] = create_tween().set_parallel().set_loops()
 
 	# Positive
-	tween.tween_property(param_angle_y, "value", -motion_range, bob_interval)
-	tween.set_parallel().tween_property(param_body_angle_y, "value", -motion_range, bob_interval)
+	tween[axis].tween_property(get(param), "value", -motion_range * motion_modifier, bob_interval * time_modifier)
+	tween[axis].tween_property(get(body_param), "value", -motion_range * motion_modifier, bob_interval * time_modifier)
 
 	# Negative
-	tween.chain().tween_property(param_angle_y, "value", motion_range, bob_interval)
-	tween.set_parallel().tween_property(param_body_angle_y, "value", motion_range, bob_interval)
+	tween[axis].chain().tween_property(get(param), "value", motion_range * motion_modifier, bob_interval * time_modifier)
+	tween[axis].tween_property(get(body_param), "value", motion_range * motion_modifier, bob_interval * time_modifier)
+
+func _stop(axis: String) -> void:
+	var time_modifier := _time_modifier(axis) * 2.0
+
+	if tween.has(axis):
+		tween[axis].kill()
+
+	tween[axis] = create_tween()
+	tween[axis].tween_property(get("param_angle_" + axis), "value", 0, bob_interval * time_modifier)
+	tween[axis].tween_property(get("param_body_angle_" + axis), "value", 0, bob_interval * time_modifier)
+
+func _time_modifier(axis: String) -> float:
+	var time_modifier := 1.0
+	match axis:
+		"x":
+			time_modifier = 2.0
+		"z":
+			time_modifier = 4.0
+		_:
+			time_modifier = 1.0
+
+	return time_modifier
+
+func _random_motion() -> Array:
+	return [
+		["x", "y", "z"],
+		["x", "y"],
+		["y", "z"],
+		["y"],
+	].pick_random()
